0cb2a58896abeb4483b69fd62029a04b68904d82
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2016-08-19 11:37:14 -0300

    Feed View Controller added

---
+++ Uknown/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewController.swift
+//  Uknown
+//
+//  Created by Vinicius Araujo de Albuquerque on 8/19/16.
+//  Copyright © 2016 uknown. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+    }
+    
+}
